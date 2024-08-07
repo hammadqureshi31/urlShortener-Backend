82a70aef0d254e9bbd1b783b6dd9c15ead9e79f7
hammadqureshi31 <154003051+hammadqureshi31@users.noreply.github.com>
2024-08-08 02:07:23 +0500

    Update consfig.js

--- consfig.js
+++ consfig.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const port = process.env.PORT || 3000;
-export const mongoDB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/urlShortener";
+export const mongoDB_URL = process.env.MONGODB_URL || "mongodb+srv://hammadqureshi:Hammad_qureshi-190@cluster0.cjcec1p.mongodb.net/";
